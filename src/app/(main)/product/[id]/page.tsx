30414a51a626681845925a54d226407646f9ee0c
cbravoquiroz <cbravo@talentsourcecorp.com>
2025-04-22 02:25:14 -0500

    fix: update prop typing in ProductDetailPage to match App Router requirements

--- src/app/(main)/product/[id]/page.tsx
+++ src/app/(main)/product/[id]/page.tsx
@@ -1,22 +1,20 @@
 import { fetchMockProducts } from "@/data/data-products";
 import { ProductDetailView } from "@/views/product-detail-view";
 import { notFound } from "next/navigation";
-import { Metadata } from "next";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export const metadata: Metadata = {
+// metadata
+export const metadata = {
   title: "Mi Banco - Detalle del Producto",
   description: "Detalles del producto financiero.",
 };
 
-export default async function ProductDetailPage({ params }: Props) {
+type Params = Promise<{ id: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { id } = await params;
+  
   const products = await fetchMockProducts();
-  const product = products.find((p) => p.id === params.id);
+  const product = products.find((p) => p.id === id);
 
   if (!product) return notFound();
 
